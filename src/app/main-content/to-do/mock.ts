df03f65a5117eb51262c883a5f5a395039f76c15
Milan <banjacm82@gmail.com>
2021-02-12 14:58:19 +0100

    done todos list added

---
+++ src/app/main-content/to-do/mock.ts
@@ -0,0 +1,34 @@
+export const todos = [
+  {
+    content: 'I dont have to push "Skra" button to make a memo - I just start typing and I "confirm" the memo by using the skra button or by pressing Enter on the keyboard',
+    completed: false
+  },
+  {
+    content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    completed: false
+  },
+  {
+    content: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    completed: false
+  },
+  {
+    content: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    completed: false
+  },
+  {
+    content: 'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.',
+    completed: false
+  },
+  {
+    content: 'The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    completed: false
+  },
+  {
+    content: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    completed: false
+  },
+  {
+    content: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    completed: false
+  }
+];
--- src/app/main-content/to-do/todos.component.ts
+++ src/app/main-content/to-do/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './../../models/Todo';
+import { todos } from './mock';
 
 @Component({
   selector: 'app-todos',
@@ -8,51 +9,46 @@ import { Todo } from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
 
+  status: boolean;
+
   todos: Todo[];
+
+  completedTodos: Todo[] = [];
 
   inputTodo = '';
 
   constructor() { }
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        content: 'I dont have to push "Skra" button to make a memo - I just start typing and I "confirm" the memo by using the skra button or by pressing Enter on the keyboard',
-        completed: false
-      },
-      {
-        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-        completed: false
-      },
-      {
-        content: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-        completed: false
-      },
-      {
-        content: 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
-        completed: false
-      },
-      {
-        content: 'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.',
-        completed: false
-      },
-      {
-        content: 'The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        completed: false
-      }
-    ]
+    this.todos = todos;
   }
 
-  toggleDone(id: number) {
+  toggleDone(index: number) {
     this.todos.map((v, i) => {
-      if (i === id) { v.completed = !v.completed; }
-
-      return v;
+      if (i === index) {
+        v.completed = !v.completed;
+        this.completedTodos.push(v);
+        this.todos.splice(i, 1);
+      }
     });
   }
 
-  deleteTodo(id: number) {
-    this.todos = this.todos.filter((v, i) => i !== id);
+  toggleCompletedDone(index: number) {
+    this.completedTodos.map((v, i) => {
+      if (i === index) {
+        v.completed = !v.completed;
+        this.todos.push(v);
+        this.completedTodos.splice(i, 1);
+      }
+    });
+  }
+
+  deleteTodo(id: number, completed?:boolean) {
+    if(completed){
+      this.completedTodos = this.completedTodos.filter((v, i) => i !== id);
+    } else{
+      this.todos = this.todos.filter((v, i) => i !== id);
+    }
   }
 
   addTodo() {
