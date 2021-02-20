4453a586fa9375b81bd2f7c00665f96b9b1e7c91
Milan <banjacm82@gmail.com>
2021-02-20 23:08:34 +0100

    dropdown menu closing function added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   status: boolean;
+  windowClick: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.windowClick = (e) => {
+      if (!e.target.matches('.main-menu')){
+        const myDropdown = document.getElementById('main-menu');
+        if (myDropdown.classList.contains('main-menu--open')) {
+          myDropdown.classList.remove('main-menu--open');
+          this.status = false;
+        }
+      }
+    };
+    window.addEventListener('click', this.windowClick);
+  }
+
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnDestroy() {
+    window.removeEventListener('click', this.windowClick);
   }
 
 }
